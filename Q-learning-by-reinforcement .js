285b12734f54384b94a7bd98ca080306455078bf
ocj94 <64177699+ocj94@users.noreply.github.com>
2023-02-20 02:31:46 +0100

    Update Q-learning-by-reinforcement .js

--- Q-learning-by-reinforcement .js
+++ Q-learning-by-reinforcement .js
@@ -1,9 +1,9 @@
- // Define the environment
+// Define the environment
 const environment = {
-  states: [
-    blackPositions = ['a1', 'a2', 'b1', 'b2', 'b3', 'c2', 'c3', 'g7', 'g8', 'h7', 'h8', 'h9', 'i8', 'i9'],
-    whitePositions = ['a4', 'a5', 'b4', 'b5', 'b6', 'c5', 'c6', 'g4', 'g5', 'h4', 'h5', 'h6', 'i5', 'i6'],
-    allPositions = [
+  states: {
+    blackPositions: ['a1', 'a2', 'b1', 'b2', 'b3', 'c2', 'c3', 'g7', 'g8', 'h7', 'h8', 'h9', 'i8', 'i9'],
+    whitePositions: ['a4', 'a5', 'b4', 'b5', 'b6', 'c5', 'c6', 'g4', 'g5', 'h4', 'h5', 'h6', 'i5', 'i6'],
+    allPositions: [
       ['i5', 'i6', 'i7', 'i8', 'i9'],
       ['h4', 'h5', 'h6', 'h7', 'h8', 'h9'],
       ['g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9'],
@@ -13,17 +13,106 @@ const environment = {
       ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7'],
       ['b1', 'b2', 'b3', 'b4', 'b5', 'b6'],
       ['a1', 'a2', 'a3', 'a4', 'a5']
-    ],
-  ],
+    ]
+  },
 
-tate: {
-    black: blackPositions,
-    white: whitePositions
+   state: {
+    black: ['a1', 'a2', 'b1', 'b2', 'b3', 'c2', 'c3', 'g7', 'g8', 'h7', 'h8', 'h9', 'i8', 'i9'],
+    white: ['a4', 'a5', 'b4', 'b5', 'b6', 'c5', 'c6', 'g4', 'g5', 'h4', 'h5', 'h6', 'i5', 'i6']
   },
 
   actions: ['move', 'push'],  // all possible actions the agent can take
   rewards: [[0, 1], [-1, 0]],  // reward matrix (state x action)
   transition: [[1, 0], [0, 2], [1, 2]],  // transition matrix (state x action)
+ 
+ function randomMove() {
+  // Chooses a random direction
+  const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+  const direction = directions[Math.floor(Math.random() * directions.length)];
+
+  // Returns the corresponding move string
+  return "m " + direction;
+}
+
+function randomPush() {
+  // Chooses a random direction
+  const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+  const direction = directions[Math.floor(Math.random() * directions.length)];
+
+  // Returns the corresponding push string
+  return "p " + direction;
+}
+
+function applyPush(state, direction) {
+  // Get the player's current position and the position in front of the player
+  const player_pos = state.position;
+  let [player_x, player_y] = player_pos.split('');
+  player_y = parseInt(player_y);
+  let [box_x, box_y] = player_pos.split('');
+  box_y = parseInt(box_y);
+
+  if (direction === 'N') {
+    box_y -= 1;
+  } else if (direction === 'NE') {
+    box_x = String.fromCharCode(box_x.charCodeAt(0) + 1);
+    box_y -= 1;
+  } else if (direction === 'E') {
+    box_x = String.fromCharCode(box_x.charCodeAt(0) + 1);
+  } else if (direction === 'SE') {
+    box_x = String.fromCharCode(box_x.charCodeAt(0) + 1);
+    box_y += 1;
+  } else if (direction === 'S') {
+    box_y += 1;
+  } else if (direction === 'SW') {
+    box_x = String.fromCharCode(box_x.charCodeAt(0) - 1);
+    box_y += 1;
+  } else if (direction === 'W') {
+    box_x = String.fromCharCode(box_x.charCodeAt(0) - 1);
+  } else if (direction === 'NW') {
+    box_x = String.fromCharCode(box_x.charCodeAt(0) - 1);
+    box_y -= 1;
+  }
+
+  // If there is no box in front of the player, return the original state
+  if (!state.boxes.includes(box_x + box_y)) {
+    return state;
+  }
+
+  // Get the new position of the box in the direction of the push
+  let new_box_x, new_box_y;
+  if (direction === 'N') {
+    new_box_x = box_x;
+    new_box_y = box_y - 1;
+  } else if (direction === 'NE') {
+    new_box_x = String.fromCharCode(box_x.charCodeAt(0) + 1);
+    new_box_y = box_y - 1;
+  } else if (direction === 'E') {
+    new_box_x = String.fromCharCode(box_x.charCodeAt(0) + 1);
+    new_box_y = box_y;
+  } else if (direction === 'SE') {
+    new_box_x = String.fromCharCode(box_x.charCodeAt(0) + 1);
+    new_box_y = box_y + 1;
+  } else if (direction === 'S') {
+    new_box_x = box_x;
+    new_box_y = box_y + 1;
+  } else if (direction === 'SW') {
+    new_box_x = String.fromCharCode(box_x.charCodeAt(0) - 1);
+    new_box_y = box_y + 1;
+  } else if (direction === 'W') {
+    new_box_x = String.fromCharCode(box_x.charCodeAt(0) - 1);
+    new_box_y = box_y;
+  } else if (direction === 'NW') {
+    new_box_x = String.fromCharCode(box_x.charCodeAt(0) - 1);
+    new_box_y = box_y - 1;
+  }
+
+  // If the box can't be pushed in the direction of the push, return the original state
+  if (state.boxes.includes(new_box_x + new_box_y) || state.walls.includes(new_box_x + new_box_y)) {
+    return state;
+  }
+
+  // Update the state with the new positions of the player and the box
+  let new_boxes = state.boxes.filter(pos => pos !== box_x + box
 
 
   isGameOver: function (state) {
@@ -63,6 +152,7 @@ tate: {
 
     return reward;
   }
+};
 
 // Initialize the Q table with zeros
 let Q = {};
