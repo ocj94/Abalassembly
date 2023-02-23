6774494d2de361a40c0920360b283d87c2b345d9
ocj94 <64177699+ocj94@users.noreply.github.com>
2023-02-23 03:04:19 +0100

    Update service-worker.js
    Cache versioning: You can manage different cache versions by creating a constant for the cache name and incrementing the version number each time you update your service worker. This way, you can ensure that users always get the latest version of your application.
    
    Cache strategy: You can define different strategies for caching files based on their type or size. For example, you can decide to cache all HTML files but only cache images smaller than 1 MB.
    
    Update management: You can add an update management function to automatically download the new version of your application when the user connects to your site.
    
    Error handling: You can add error handlers to manage situations where caching or file retrieval fails.
    
    File preloading: You can add a preloading function to preload specific files so that they are available more quickly when the user needs them.
    
    Push notification: You can add a push notification feature to send notifications to the user even when the application is not open.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,7 @@
-const CACHE_NAME = 'Abalassembly-cache-v2';
+// Manage different cache versions
+const CACHE_NAME = 'Abalassembly-cache-v3';
+
+// Define URLs to be cached
 const urlsToCache = [
 './',
 './index.html',
@@ -8,24 +11,97 @@ const urlsToCache = [
 './font.woff2'
 ];
 
-self.addEventListener('install', event => {
+// Install event listener
+self.addEventListener('install', function(event) {
 event.waitUntil(
 caches.open(CACHE_NAME)
-.then(cache => {
+.then(function(cache) {
 console.log('Opened cache');
 return cache.addAll(urlsToCache);
 })
 );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch event listener
+self.addEventListener('fetch', function(event) {
 event.respondWith(
 caches.match(event.request)
-.then(response => {
+.then(function(response) {
 if (response) {
 return response;
 }
 return fetch(event.request);
 })
+);
+});
+
+// Update event listener
+self.addEventListener('activate', function(event) {
+const cacheWhitelist = [CACHE_NAME];
+
+event.waitUntil(
+caches.keys().then(function(cacheNames) {
+return Promise.all(
+cacheNames.map(function(cacheName) {
+if (cacheWhitelist.indexOf(cacheName) === -1) {
+return caches.delete(cacheName);
+}
+})
+);
+})
+);
+});
+
+// Cache strategies
+self.addEventListener('fetch', function(event) {
+if (event.request.url.endsWith('.jpg')) {
+event.respondWith(
+caches.open('image-cache').then(function(cache) {
+return cache.match(event.request).then(function(response) {
+return response || fetch(event.request).then(function(response) {
+cache.put(event.request, response.clone());
+return response;
+});
+});
+})
+);
+} else {
+event.respondWith(
+caches.match(event.request).then(function(response) {
+return response || fetch(event.request);
+})
+);
+}
+});
+
+// Error handling
+self.addEventListener('error', function(event) {
+console.error('Error:', event.error);
+});
+
+// Preload files
+const filesToPreload = [
+'./about.html',
+'./contact.html',
+'./services.html'
+];
+
+self.addEventListener('install', function(event) {
+event.waitUntil(
+caches.open(CACHE_NAME).then(function(cache) {
+return cache.addAll(filesToPreload);
+})
+);
+});
+
+// Push notifications
+self.addEventListener('push', function(event) {
+const title = 'New notification';
+const options = {
+body: 'This is a push notification',
+icon: './notification-icon.png'
+};
+event.waitUntil(
+self.registration.showNotification(title, options)
 );
 });
